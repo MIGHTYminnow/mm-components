923e77f37567f18beb78f88b0efefe2046fe0115
Braad <braad@mightyminnow.com>
2015-08-19 17:18:32 -0700

    Switch away from using extract in our custom heading component and refactor

--- components/custom-heading.php
+++ components/custom-heading.php
@@ -20,8 +20,8 @@ add_shortcode( 'mm_custom_heading', 'mm_custom_heading_shortcode' );
  */
 function mm_custom_heading_shortcode( $atts, $content = null, $tag ) {
 
-	extract( mm_shortcode_atts( array(
-		'heading'			=> 'h2',
+	$atts = mm_shortcode_atts( array(
+		'heading'			=> '',
 		'font_family'		=> '',
 		'font_size'			=> '',
 		'margin_bottom'		=> '',
@@ -29,46 +29,77 @@ function mm_custom_heading_shortcode( $atts, $content = null, $tag ) {
 		'text_transform'	=> '',
 		'text_align'		=> '',
 		'link'				=> '',
-	), $atts ) );
+	), $atts );
 
-	// Clean up content - this is necessary
+	$heading = ( '' !== $atts['heading'] ) ? (string)$atts['heading'] : 'h2';
+	$font_family = ( '' !== $atts['font_family'] ) ? (string)$atts['font_family'] : '';
+	$font_size = ( '' !== $atts['font_size'] ) ? (string)$atts['font_size'] : '';
+	$margin_bottom = ( '' !== $atts['margin_bottom'] ) ? $atts['margin_bottom'] : '';
+	$color = ( '' !== $atts['color'] ) ? (string)$atts['color'] : '';
+	$text_transform = ( '' !== $atts['text_transform'] ) ? (string)$atts['text_transform'] : '';
+	$text_align = ( '' !== $atts['text_align'] ) ? (string)$atts['text_align'] : '';
+	$link = ( '' !== $atts['link'] ) ? (string)$atts['link'] : '';
+
+	// Clean up content - this is necessary.
 	//$content = wpb_js_remove_wpautop( $content, true );
-
-	// Process margin_bottom into a px value, if it was set
-	if ( '' !== $margin_bottom ) {
-		$margin_bottom = 'margin-bottom: ' . (int)$margin_bottom . 'px;';
-	}
 
 	// Get link array [url, title, target]
 	$link_array = vc_build_link( $link );
 
+	// Wrap the heading in a link if one was passed in.
 	if ( isset( $link_array['url'] ) && ! empty( $link_array['url'] ) ) {
 		$content = '<a href="' . $link_array['url'] . '" title="' . $link_array['title'] . '">' . $content . '</a>';
 	}
 
-	// Get Mm classes
+	// Get Mm classes.
 	$mm_classes = str_replace( '_', '-', $tag );
 	$mm_classes = apply_filters( 'mm_shortcode_custom_classes', $mm_classes, $tag, $atts );
 
-	// Create heading classes
-	$class = implode( ' ', array(
-		'font-family-' . $font_family,
-		'font-size-' . $font_size,
-		'color-' . $color,
-		'text-transform-' . $text_transform,
-		'text-align-' . $text_align,
-	) );
+	// Set up our classes array.
+	$classes = array();
 
-	$output = sprintf( '<%s class="%s" style="%s">%s</%s>',
+	if ( '' !== $font_family ) {
+		$classes[] = 'font-family-' . $font_family;
+	}
+	if ( '' !== $font_family ) {
+		$classes[] = 'font-family-' . $font_family;
+	}
+	if ( '' !== $font_size ) {
+		$classes[] = 'font-size-' . $font_size;
+	}
+	if ( '' !== $color ) {
+		$classes[] = 'color-' . $color;
+	}
+	if ( '' !== $text_transform ) {
+		$classes[] = 'text-transform-' . $text_transform;
+	}
+	if ( '' !== $text_align ) {
+		$classes[] = 'text-align-' . $text_align;
+	}
+
+	// Build our string of classes.
+	$classes = implode( ' ', $classes );
+
+	// Set up our styles array.
+	$styles = array();
+
+	if ( '' !== $margin_bottom ) {
+		$styles[] = 'margin-bottom: ' . (int)$margin_bottom . 'px;';
+	}
+
+	// Build our string of styles.
+	$styles = implode( ' ', $styles );
+	$style = ( '' !== $styles ) ? 'style="' . $styles . '"' : '';
+
+	$output = sprintf( '<%s class="%s" %s>%s</%s>',
 		$heading,
-		$mm_classes . ' ' . $class,
-		$margin_bottom,
+		$mm_classes . ' ' . $classes,
+		$style,
 		$content,
 		$heading
 	);
 
 	return $output;
-
 }
 
 add_action( 'vc_before_init', 'mm_vc_custom_heading' );
