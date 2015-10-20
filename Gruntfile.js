58ee9b965179d24af4fb6d44d4876a38bbd49c91
Braad Martin <work@braadmartin.com>
2015-10-19 23:02:17 -0700

    Update Gruntfile and package.json with better tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,15 @@
-module.exports = function(grunt) {
+module.exports = function( grunt ) {
 
-	// Project configuration.
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON( 'package.json' ),
 		sass: {
 			dist: {
 				options: {
-					style: 'expanded'
+					sourceMap: true,
+					outputStyle: 'expanded'
 				},
 				files: {
+					'css/mm-components-admin.css' : 'scss/mm-components-admin.scss',
 					'css/mm-components-public.css' : 'scss/mm-components-public.scss'
 				}
 			}
@@ -19,30 +20,43 @@ module.exports = function(grunt) {
 				tasks: ['sass'],
 				options: {
 					livereload: true,
-				}
-			}
+				},
+			},
 		},
 		postcss: {
 			options: {
 				processors: [
-					require('pixrem')(), // Adds pixel fallbacks for rem units.
-					require('autoprefixer')({browsers: 'last 2 versions'}), // Adds vendor prefixes where required; removes redundant prefixes.
-					require('cssnano')() // Optimizes and minifies resulting CSS output.
+					require( 'autoprefixer' )({ browsers: 'last 2 versions' }), // Add vendor prefixes.
 				]
 			},
 			dist: {
 				src: 'css/*.css'
 			}
+		},
+		wp_readme_to_markdown: {
+			your_target: {
+				files: {
+					'readme.md': 'readme.txt'
+				}
+			}
+		},
+		makepot: {
+			target: {
+				options: {
+					type: 'wp-plugin'
+				}
+			}
 		}
-
 	});
 
-	// Load Grunt plugins.
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-postcss');
+	// Load plugins.
+	grunt.loadNpmTasks( 'grunt-sass' );
+	grunt.loadNpmTasks( 'grunt-postcss' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown' );
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
 
-	// Configure default task(s).
-	grunt.registerTask('default', ['watch']);
+	// Configure tasks.
+	grunt.registerTask( 'build', ['sass', 'postcss', 'wp_readme_to_markdown', 'makepot'] );
 
 };
