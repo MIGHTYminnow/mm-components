ba345b89db82f3a2cd6ca033ba79549ba0f6529d
Braad Martin <work@braadmartin.com>
2015-11-23 08:00:39 -0800

    Update Mm Icon Box to use new component structure, add icon size param, and remove pixelicons

--- components/icon-box.php
+++ components/icon-box.php
@@ -8,50 +8,59 @@
  * @since   1.0.0
  */
 
-add_shortcode( 'mm_icon_box', 'mm_icon_box_shortcode' );
 /**
- * Output Icon Box.
+ * Build and return the Icon Box component.
  *
- * @since  1.0.0
+ * @since   1.0.0
  *
- * @param   array  $atts  Shortcode attributes.
+ * @param   array  $args  The args.
  *
- * @return  string        Shortcode output.
+ * @return  string        The HTML.
  */
-function mm_icon_box_shortcode( $atts, $content = null, $tag ) {
+function mm_icon_box( $args ) {
 
-	$atts = mm_shortcode_atts( array(
+	$component = 'mm-icon-box';
+
+	// Set our defaults and use them as needed.
+	$defaults = array(
 		'icon_type'        => 'fontawesome',
 		'icon_fontawesome' => '',
 		'icon_openiconic'  => '',
 		'icon_typicons'    => '',
 		'icon_entypo'      => '',
 		'icon_linecons'    => '',
-		'icon_pixelicons'  => '',
+		'icon_size'        => 'normal',
 		'heading_text'     => '',
+		'content'          => '',
 		'link'             => '',
-		'link_text'        => __( 'Read more', 'mm-components' ),
+		'link_text'        => '',
 		'link_title'       => '',
 		'link_target'      => '',
-	), $atts );
+	);
+	$args = wp_parse_args( (array)$args, $defaults );
 
-	// Fix wpautop issues in $content.
-	if ( function_exists( 'wpb_js_remove_wpautop' ) ) {
-		$content = wpb_js_remove_wpautop( $content, true );
-	}
+	// Get clean param values.
+	$icon_type        = $args['icon_type'];
+	$icon_fontawesome = $args['icon_fontawesome'];
+	$icon_openiconic  = $args['icon_openiconic'];
+	$icon_typicons    = $args['icon_typicons'];
+	$icon_entypo      = $args['icon_entypo'];
+	$icon_linecons    = $args['icon_linecons'];
+	$icon_size        = $args['icon_size'];
+	$heading_text     = $args['heading_text'];
+	$content          = $args['content'];
+	$link_url         = '';
+	$link_title       = '';
+	$link_target      = '';
 
 	// Handle a raw link or a VC link array.
-	$link_url    = '';
-	$link_title  = '';
-	$link_target = '';
+	if ( ! empty( $args['link'] ) ) {
 
-	if ( ! empty( $atts['link'] ) ) {
-
-		if ( 'url' === substr( $atts['link'], 0, 3 ) ) {
+		if ( 'url' === substr( $args['link'], 0, 3 ) ) {
 
 			if ( function_exists( 'vc_build_link' ) ) {
 
-				$link_array  = vc_build_link( $atts['link'] );
+				$link_array  = vc_build_link( $args['link'] );
 				$link_url    = $link_array['url'];
 				$link_title  = $link_array['title'];
 				$link_target = $link_array['target'];
@@ -59,36 +68,41 @@ function mm_icon_box_shortcode( $atts, $content = null, $tag ) {
 
 		} else {
 
-			$link_url    = $atts['link'];
-			$link_title  = $atts['link_title'];
-			$link_target = $atts['link_target'];
+			$link_url    = $args['link'];
+			$link_title  = $args['link_title'];
+			$link_target = $args['link_target'];
 		}
 	}
 
+	// Fix wpautop issues in $content.
+	if ( function_exists( 'wpb_js_remove_wpautop' ) ) {
+		$content = wpb_js_remove_wpautop( $content, true );
+	}
+
 	// Get Mm classes.
-	$mm_classes = apply_filters( 'mm_components_custom_classes', '', $tag, $atts );
+	$mm_classes = apply_filters( 'mm_components_custom_classes', '', $component, $args );
 
-	// Get icon type.
-	$icon_type = $atts['icon_type'];
+	// Add icon size class.
+	if ( $icon_size ) {
+		$mm_classes .= ' icon-size-' . $icon_size;
+	}
 
+	// Get the icon classes.
 	switch ( $icon_type ) {
 		case 'fontawesome':
-			$icon = ! empty( $atts['icon_fontawesome'] ) ? $atts['icon_fontawesome'] : 'fa fa-adjust';
+			$icon = ! empty( $icon_fontawesome ) ? $icon_fontawesome : 'fa fa-adjust';
 			break;
 		case 'openiconic':
-			$icon = $atts['icon_openiconic'];
+			$icon = $icon_openiconic;
 			break;
 		case 'typicons':
-			$icon = $atts['icon_typicons'];
+			$icon = $icon_typicons;
 			break;
 		case 'entypo':
-			$icon = $atts['icon_entypo'];
+			$icon = $icon_entypo;
 			break;
 		case 'linecons':
-			$icon = $atts['icon_linecons'];
-			break;
-		case 'pixelicons':
-			$icon = $atts['icon_pixelicons'];
+			$icon = $icon_linecons;
 			break;
 		default:
 			$icon = 'fa fa-adjust';
@@ -101,36 +115,52 @@ function mm_icon_box_shortcode( $atts, $content = null, $tag ) {
 
 	ob_start(); ?>
 
-	<div class="<?php echo $mm_classes; ?>">
+	<div class="<?php echo esc_attr( $mm_classes ); ?>">
 
-		<i class="mm-icon <?php echo $icon; ?>"></i>
+		<i class="mm-icon <?php echo esc_attr( $icon ); ?>"></i>
 
 		<?php if ( ! empty( $atts['heading_text'] ) ) : ?>
-			<h3><?php echo $atts['heading_text']; ?></h3>
+			<h3 class="icon-box-heading"><?php echo esc_html( $atts['heading_text'] ); ?></h3>
 		<?php endif; ?>
 
 		<?php if ( $content ) : ?>
-			<?php echo $content; ?>
+			<div class="icon-box-content"><?php echo wp_kses_post( $content ); ?></div>
 		<?php endif; ?>
 
-		<?php
-		if ( ! empty( $link_text ) && ! empty( $link_url ) ) {
-			printf( '<a href="%s" title="%s" target="%s" class="more-link">%s</a>',
-				$link_url,
-				$link_title,
-				$link_target,
-				$atts['link_text']
+		<?php if ( ! empty( $link_text ) && ! empty( $link_url ) ) {
+			printf( '<a href="%s" title="%s" target="%s" class="icon-box-link">%s</a>',
+				esc_url( $link_url ),
+				esc_attr( $link_title ),
+				esc_attr( $link_target ),
+				esc_html( $atts['link_text'] )
 			);
-		}
-		?>
+		} ?>
 
 	</div>
 
 	<?php
 
-	$output = ob_get_clean();
+	return ob_get_clean();
+}
 
-	return $output;
+add_shortcode( 'mm_icon_box', 'mm_icon_box_shortcode' );
+/**
+ * Icon Box Shortcode.
+ *
+ * @since  1.0.0
+ *
+ * @param   array   $atts     Shortcode attributes.
+ * @param   string  $content  Shortcode content.
+ *
+ * @return  string            Shortcode output.
+ */
+function mm_icon_box_shortcode( $atts = array(), $content = null ) {
+
+	if ( $content ) {
+		$atts['content'] = $content;
+	}
+
+	return mm_icon_box( $atts );
 }
 
 add_action( 'vc_before_init', 'mm_vc_icon_box' );
@@ -141,175 +171,129 @@ add_action( 'vc_before_init', 'mm_vc_icon_box' );
  */
 function mm_vc_icon_box() {
 
-	// $pixel_icons is needed because VC.
-	global $pixel_icons;
-	$pixel_icons = array(
-		array( 'vc_pixel_icon vc_pixel_icon-alert' => 'Alert' ),
-		array( 'vc_pixel_icon vc_pixel_icon-info' => 'Info' ),
-		array( 'vc_pixel_icon vc_pixel_icon-tick' => 'Tick' ),
-		array( 'vc_pixel_icon vc_pixel_icon-explanation' => 'Explanation' ),
-		array( 'vc_pixel_icon vc_pixel_icon-address_book' => 'Address book' ),
-		array( 'vc_pixel_icon vc_pixel_icon-alarm_clock' => 'Alarm clock' ),
-		array( 'vc_pixel_icon vc_pixel_icon-anchor' => 'Anchor' ),
-		array( 'vc_pixel_icon vc_pixel_icon-application_image' => 'Application Image' ),
-		array( 'vc_pixel_icon vc_pixel_icon-arrow' => 'Arrow' ),
-		array( 'vc_pixel_icon vc_pixel_icon-asterisk' => 'Asterisk' ),
-		array( 'vc_pixel_icon vc_pixel_icon-hammer' => 'Hammer' ),
-		array( 'vc_pixel_icon vc_pixel_icon-balloon' => 'Balloon' ),
-		array( 'vc_pixel_icon vc_pixel_icon-balloon_buzz' => 'Balloon Buzz' ),
-		array( 'vc_pixel_icon vc_pixel_icon-balloon_facebook' => 'Balloon Facebook' ),
-		array( 'vc_pixel_icon vc_pixel_icon-balloon_twitter' => 'Balloon Twitter' ),
-		array( 'vc_pixel_icon vc_pixel_icon-battery' => 'Battery' ),
-		array( 'vc_pixel_icon vc_pixel_icon-binocular' => 'Binocular' ),
-		array( 'vc_pixel_icon vc_pixel_icon-document_excel' => 'Document Excel' ),
-		array( 'vc_pixel_icon vc_pixel_icon-document_image' => 'Document Image' ),
-		array( 'vc_pixel_icon vc_pixel_icon-document_music' => 'Document Music' ),
-		array( 'vc_pixel_icon vc_pixel_icon-document_office' => 'Document Office' ),
-		array( 'vc_pixel_icon vc_pixel_icon-document_pdf' => 'Document PDF' ),
-		array( 'vc_pixel_icon vc_pixel_icon-document_powerpoint' => 'Document Powerpoint' ),
-		array( 'vc_pixel_icon vc_pixel_icon-document_word' => 'Document Word' ),
-		array( 'vc_pixel_icon vc_pixel_icon-bookmark' => 'Bookmark' ),
-		array( 'vc_pixel_icon vc_pixel_icon-camcorder' => 'Camcorder' ),
-		array( 'vc_pixel_icon vc_pixel_icon-camera' => 'Camera' ),
-		array( 'vc_pixel_icon vc_pixel_icon-chart' => 'Chart' ),
-		array( 'vc_pixel_icon vc_pixel_icon-chart_pie' => 'Chart pie' ),
-		array( 'vc_pixel_icon vc_pixel_icon-clock' => 'Clock' ),
-		array( 'vc_pixel_icon vc_pixel_icon-fire' => 'Fire' ),
-		array( 'vc_pixel_icon vc_pixel_icon-heart' => 'Heart' ),
-		array( 'vc_pixel_icon vc_pixel_icon-mail' => 'Mail' ),
-		array( 'vc_pixel_icon vc_pixel_icon-play' => 'Play' ),
-		array( 'vc_pixel_icon vc_pixel_icon-shield' => 'Shield' ),
-		array( 'vc_pixel_icon vc_pixel_icon-video' => 'Video' ),
-	);
-
 	vc_map( array(
-		'name' => __( 'Icon Box', 'mm-components' ),
-		'base' => 'mm_icon_box',
-		'class' => '',
-		'icon' => MM_COMPONENTS_ASSETS_URL . 'component-icon.png',
+		'name'     => __( 'Icon Box', 'mm-components' ),
+		'base'     => 'mm_icon_box',
+		'class'    => '',
+		'icon'     => MM_COMPONENTS_ASSETS_URL . 'component-icon.png',
 		'category' => __( 'Content', 'mm-components' ),
-		'params' => array(
+		'params'   => array(
 			array(
-				'type' => 'dropdown',
+				'type'    => 'dropdown',
 				'heading' => __( 'Icon library', 'mm-components' ),
-				'value' => array(
+				'value'   => array(
 					__( 'Font Awesome', 'mm-components' ) => 'fontawesome',
-					__( 'Open Iconic', 'mm-components' ) => 'openiconic',
-					__( 'Typicons', 'mm-components' ) => 'typicons',
-					__( 'Entypo', 'mm-components' ) => 'entypo',
-					__( 'Linecons', 'mm-components' ) => 'linecons',
-					__( 'Pixel', 'mm-components' ) => 'pixelicons',
+					__( 'Open Iconic', 'mm-components' )  => 'openiconic',
+					__( 'Typicons', 'mm-components' )     => 'typicons',
+					__( 'Entypo', 'mm-components' )       => 'entypo',
+					__( 'Linecons', 'mm-components' )     => 'linecons',
 				),
-				'param_name' => 'icon_type',
-				'description' => __( 'Select icon library.', 'mm-components' ),
+				'param_name'  => 'icon_type',
+				'description' => __( 'Select an icon library.', 'mm-components' ),
 			),
 			array(
-				'type' => 'iconpicker',
-				'heading' => __( 'Icon', 'mm-components' ),
+				'type'       => 'iconpicker',
+				'heading'    => __( 'Icon', 'mm-components' ),
 				'param_name' => 'icon_fontawesome',
-				'settings' => array(
-					'emptyIcon' => false, // default true, display an "EMPTY" icon?
+				'settings'   => array(
+					'emptyIcon'    => false, // default true, display an "EMPTY" icon?
 					'iconsPerPage' => 200, // default 100, how many icons per/page to display
 				),
-				'dependency' => array(
+				'dependency'  => array(
 					'element' => 'icon_type',
-					'value' => 'fontawesome',
+					'value'   => 'fontawesome',
 				),
-				'description' => __( 'Select icon from library.', 'mm-components' ),
+				'description' => __( 'Select an icon from the library.', 'mm-components' ),
 			),
 			array(
-				'type' => 'iconpicker',
-				'heading' => __( 'Icon', 'mm-components' ),
+				'type'       => 'iconpicker',
+				'heading'    => __( 'Icon', 'mm-components' ),
 				'param_name' => 'icon_openiconic',
-				'settings' => array(
-					'emptyIcon' => false, // default true, display an "EMPTY" icon?
-					'type' => 'openiconic',
-					'iconsPerPage' => 200, // default 100, how many icons per/page to display
+				'settings'   => array(
+					'emptyIcon'    => false,
+					'type'         => 'openiconic',
+					'iconsPerPage' => 200,
 				),
-				'dependency' => array(
+				'dependency'  => array(
 					'element' => 'icon_type',
-					'value' => 'openiconic',
+					'value'   => 'openiconic',
 				),
-				'description' => __( 'Select icon from library.', 'mm-components' ),
+				'description' => __( 'Select an icon from the library.', 'mm-components' ),
 			),
 			array(
-				'type' => 'iconpicker',
-				'heading' => __( 'Icon', 'mm-components' ),
+				'type'       => 'iconpicker',
+				'heading'    => __( 'Icon', 'mm-components' ),
 				'param_name' => 'icon_typicons',
-				'settings' => array(
-					'emptyIcon' => false, // default true, display an "EMPTY" icon?
-					'type' => 'typicons',
-					'iconsPerPage' => 200, // default 100, how many icons per/page to display
+				'settings'   => array(
+					'emptyIcon'    => false,
+					'type'         => 'typicons',
+					'iconsPerPage' => 200,
 				),
-				'dependency' => array(
+				'dependency'  => array(
 					'element' => 'icon_type',
-					'value' => 'typicons',
+					'value'   => 'typicons',
 				),
-				'description' => __( 'Select icon from library.', 'mm-components' ),
+				'description' => __( 'Select an icon from the library.', 'mm-components' ),
 			),
 			array(
-				'type' => 'iconpicker',
-				'heading' => __( 'Icon', 'mm-components' ),
+				'type'       => 'iconpicker',
+				'heading'    => __( 'Icon', 'mm-components' ),
 				'param_name' => 'icon_entypo',
-				'settings' => array(
-					'emptyIcon' => false, // default true, display an "EMPTY" icon?
-					'type' => 'entypo',
-					'iconsPerPage' => 300, // default 100, how many icons per/page to display
+				'settings'   => array(
+					'emptyIcon'    => false,
+					'type'         => 'entypo',
+					'iconsPerPage' => 300,
 				),
-				'dependency' => array(
+				'dependency'  => array(
 					'element' => 'icon_type',
-					'value' => 'entypo',
+					'value'   => 'entypo',
 				),
 			),
 			array(
-				'type' => 'iconpicker',
-				'heading' => __( 'Icon', 'mm-components' ),
+				'type'       => 'iconpicker',
+				'heading'    => __( 'Icon', 'mm-components' ),
 				'param_name' => 'icon_linecons',
-				'settings' => array(
-					'emptyIcon' => false, // default true, display an "EMPTY" icon?
-					'type' => 'linecons',
-					'iconsPerPage' => 200, // default 100, how many icons per/page to display
+				'settings'   => array(
+					'emptyIcon'    => false,
+					'type'         => 'linecons',
+					'iconsPerPage' => 200,
 				),
-				'dependency' => array(
+				'dependency'  => array(
 					'element' => 'icon_type',
-					'value' => 'linecons',
+					'value'   => 'linecons',
 				),
-				'description' => __( 'Select icon from library.', 'mm-components' ),
+				'description' => __( 'Select an icon from the library.', 'mm-components' ),
 			),
 			array(
-				'type' => 'iconpicker',
-				'heading' => __( 'Icon', 'mm-components' ),
-				'param_name' => 'icon_pixelicons',
-				'settings' => array(
-					'emptyIcon' => false, // default true, display an "EMPTY" icon?
-					'type' => 'pixelicons',
-					'source' => $pixel_icons,
+				'type'        => 'dropdown',
+				'heading'     => __( 'Icon Box Size', 'mm-components' ),
+				'param_name'  => 'icon_size',
+				'description' => __( 'Select a general size for the icon box', 'mm-components' ),
+				'value'       => array(
+					__( 'Normal', 'mm-components' ) => 'normal',
+					__( 'Large', 'mm-components' )  => 'large',
+					__( 'Small', 'mm-components' )  => 'small',
 				),
-				'dependency' => array(
-					'element' => 'icon_type',
-					'value' => 'pixelicons',
-				),
-				'description' => __( 'Select icon from library.', 'mm-components' ),
 			),
 			array(
-				'type' => 'textfield',
-				'heading' => __( 'Heading', 'mm-components' ),
-				'param_name' => 'heading_text',
+				'type'        => 'textfield',
+				'heading'     => __( 'Heading', 'mm-components' ),
+				'param_name'  => 'heading_text',
 				'admin_label' => true,
 			),
 			array(
-				'type' => 'textarea_html',
-				'heading' => __( 'Paragraph Text', 'mm-components' ),
+				'type'       => 'textarea_html',
+				'heading'    => __( 'Paragraph Text', 'mm-components' ),
 				'param_name' => 'content',
 			),
 			array(
-				'type' => 'vc_link',
-				'heading' => __( 'Link URL', 'mm-components' ),
+				'type'       => 'vc_link',
+				'heading'    => __( 'Link URL', 'mm-components' ),
 				'param_name' => 'link',
 			),
 			array(
-				'type' => 'textfield',
-				'heading' => __( 'Link Text', 'mm-components' ),
+				'type'       => 'textfield',
+				'heading'    => __( 'Link Text', 'mm-components' ),
 				'param_name' => 'link_text',
 			),
 		)
