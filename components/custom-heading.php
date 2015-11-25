99a79a4937e58c62cb3fe2fe257d51064da273ed
carrieforde <carrie@carrieforde.com>
2015-11-25 13:10:52 -0800

    Improve Mm Custom Heading  component structure

--- components/custom-heading.php
+++ components/custom-heading.php
@@ -8,62 +8,57 @@
  * @since   1.0.0
  */
 
-add_shortcode( 'mm_custom_heading', 'mm_custom_heading_shortcode' );
 /**
- * Output Custom Heading.
+ * Build and return the Custom Heading component.
  *
- * @since  1.0.0
+ * @since 1.0.0
  *
- * @param   array  $atts  Shortcode attributes.
+ * @param   array  $args  The args.
  *
- * @return  string        Shortcode output.
+ * @return  string        The HTML.
  */
-function mm_custom_heading_shortcode( $atts, $content = null, $tag ) {
+function mm_custom_heading( $args ) {
 
-	$atts = mm_shortcode_atts( array(
-		'heading'        => '',
-		'font_family'    => '',
-		'font_size'      => '',
-		'margin_bottom'  => '',
-		'color'          => '',
-		'text_transform' => '',
-		'text_align'     => '',
-		'link'           => '',
-		'link_title'     => '',
-		'link_target'    => '',
-	), $atts );
+	$component = 'mm-custom-heading';
 
-	$heading = ( '' !== $atts['heading'] ) ? (string)$atts['heading'] : 'h2';
-	$font_family = ( '' !== $atts['font_family'] ) ? (string)$atts['font_family'] : '';
-	$font_size = ( '' !== $atts['font_size'] ) ? (string)$atts['font_size'] : '';
-	$margin_bottom = ( '' !== $atts['margin_bottom'] ) ? $atts['margin_bottom'] : '';
-	$color = ( '' !== $atts['color'] ) ? (string)$atts['color'] : '';
-	$text_transform = ( '' !== $atts['text_transform'] ) ? (string)$atts['text_transform'] : '';
-	$text_align = ( '' !== $atts['text_align'] ) ? (string)$atts['text_align'] : '';
-	$link = ( '' !== $atts['link'] ) ? (string)$atts['link'] : '';
+	//Set our defaults and use them as needed.
+	$defaults = array(
+		'heading_text'  => '',
+		'heading'       => '',
+		'font_family'   => '',
+		'size'          => '',
+		'weight'        => '',
+		'transform'     => '',
+		'alignment'     => '',
+		'color'         => '',
+		'margin_bottom' => '',
+		'link'          => '',
+		'link_title'    => '',
+		'link_target'   => '',
+	);
+	$args = wp_parse_args( (array)$args, $defaults );
 
-	// Handle a raw link or a VC link array.
+	//Handle a raw link or VC link array.
 	$link_url    = '';
 	$link_title  = '';
 	$link_target = '';
 
-	if ( ! empty( $atts['link'] ) ) {
+	if ( ! empty( $args['link'] ) ) {
 
-		if ( 'url' === substr( $atts['link'], 0, 3 ) ) {
+		if ( 'url' === substr( $args['link'], 0, 3 ) ) {
 
-			if ( function_exists( 'vc_build_link' ) ) {
+			if (function_exists( 'vc_build_link' ) ) {
 
-				$link_array  = vc_build_link( $atts['link'] );
+				$link_array  = vc_build_link( $args['link'] );
 				$link_url    = $link_array['url'];
 				$link_title  = $link_array['title'];
 				$link_target = $link_array['target'];
 			}
-
 		} else {
 
-			$link_url    = $atts['link'];
-			$link_title  = $atts['link_title'];
-			$link_target = $atts['link_target'];
+			$link_url    = $args['link'];
+			$link_title  = $args['link_title'];
+			$link_target = $args['link_target'];
 		}
 	}
 
@@ -78,51 +73,85 @@ function mm_custom_heading_shortcode( $atts, $content = null, $tag ) {
 		);
 	}
 
-	// Get Mm classes.
-	$mm_classes = apply_filters( 'mm_components_custom_classes', '', $tag, $atts );
+	// Build the alignment class.
+	$alignment = 'mm-text-align-' . $args['alignment'];
 
-	// Set up our classes array.
+	// Set up the heading.
+	$heading = ( '' !== $args['heading'] ) ? (string)$args['heading'] : 'h2';
+
+	// Set up custom heading classes.
 	$classes = array();
+	$classes[] = 'mm-custom-heading';
+	if ( ! empty( $args['font_family'] ) ) {
+		$classes[] = $args['font_family'];
+	}
+	if ( ! empty( $args['size'] ) ) {
+		$classes[] = $args['size'];
+	}
+	if ( ! empty( $args['weight'] ) ) {
+		$classes[] = $args['weight'];
+	}
+	if ( ! empty( $args['transform'] ) ) {
+		$classes[] = $args['transform'];
+	}
+	if ( ! empty( $args['alignment'] ) ) {
+		$classes[] = $args['alignment'];
+	}
+	if ( ! empty( $args['color'] ) ) {
+		$classes[] = $args['color'];
+	}
 
-	if ( '' !== $font_family ) {
-		$classes[] = 'font-family-' . $font_family;
-	}
-	if ( '' !== $font_size ) {
-		$classes[] = 'font-size-' . $font_size;
-	}
-	if ( '' !== $color ) {
-		$classes[] = 'color-' . $color;
-	}
-	if ( '' !== $text_transform ) {
-		$classes[] = 'mm-text-transform-' . $text_transform;
-	}
-	if ( '' !== $text_align ) {
-		$classes[] = 'mm-text-align-' . $text_align;
-	}
-
-	// Build our string of classes.
 	$classes = implode( ' ', $classes );
+
+	// Get Mm classes.
+	$mm_classes = apply_filters( 'mm_components_custom_classes', '', $component, $args );
 
 	// Set up our styles array.
 	$styles = array();
-
-	if ( '' !== $margin_bottom ) {
-		$styles[] = 'margin-bottom: ' . (int)$margin_bottom . 'px;';
+	if ( ! empty( $args['margin_bottom'] ) ) {
+		$styles[] = 'margin-bottom: ' . (int)$args['margin_bottom'] . 'px;';
 	}
 
 	// Build our string of styles.
 	$styles = implode( ' ', $styles );
-	$style = ( '' !== $styles ) ? 'style="' . $styles . '"' : '';
+	$style = ( '' !== $styles ) ? 'style="' . $styles .'"' : '';
 
-	$output = sprintf( '<%s class="%s" %s>%s</%s>',
+	// Do something with the heading text.
+	$heading_text = sanitize_text_field( $args['heading_text'] );
+
+
+	// Generate the output.
+	$output = sprintf( '<%s class="%s %s %s" %s>%s</%s>',
 		$heading,
-		$mm_classes . ' ' . $classes,
+		$mm_classes,
+		$classes,
+		$alignment,
 		$style,
-		$content,
+		$heading_text,
 		$heading
 	);
 
 	return $output;
+
+}
+
+add_shortcode( 'mm_custom_heading', 'mm_custom_heading_shortcode' );
+/**
+ * Custom Heading shortcode.
+ *
+ * @since  1.0.0
+ *
+ * @param   array  $atts  Shortcode attributes.
+ *
+ * @return  string        Shortcode output.
+ */
+function mm_custom_heading_shortcode( $atts = array(), $content = null ) {
+
+	if ( $content ) {
+		$atts['heading_text'] = $content;
+	}
+
+	return mm_custom_heading( $atts );
 }
 
 add_action( 'vc_before_init', 'mm_vc_custom_heading' );
